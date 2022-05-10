e2074c1bfeb617bc4ae42071df980bc83fc450ea
quan-p <m.phamquan@gmail.com>
2022-05-10 18:33:23 -0500

    Install nodemon

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb+srv://quan155:201233@cluster0.hwrfg.mongodb.net/sff_parts?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
