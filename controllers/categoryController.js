47e90cd6af69a34369cde2b1c4e42aa78276ce18
quan-p <m.phamquan@gmail.com>
2022-05-11 16:06:43 -0500

    Create basic skeleton for both controllers to test export and imports

---
+++ controllers/categoryController.js
@@ -0,0 +1,5 @@
+var Category = require('../models/category');
+
+exports.category_list = function(req, res, next) {
+    res.send('NOT IMPLEMENTED: CATEGORY LIST');
+};
---
+++ controllers/componentController.js
@@ -0,0 +1,5 @@
+var Component = require('../models/component');
+
+exports.component_list = function(req, res, next) {
+    res.send('NOT IMPLEMENTED: COMPONENT LIST');
+};
