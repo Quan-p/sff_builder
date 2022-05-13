3975927e6a4c6e2ca854eb7f62f3959d2196cc10
quan-p <m.phamquan@gmail.com>
2022-05-13 15:02:06 -0500

    List items now link to category url

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -12,10 +12,10 @@ exports.category_list = function(req, res, next) {
     Category.find({}, 'title')
       .sort({title : 1})
     //   .populate('author')
-      .exec(function (err, category_list) {
+      .exec(function (err, list_categories) {
         if (err) { return next(err); }
         //Successful, so render
-    res.render('category_list', { title: 'Category List', category_list });
+    res.render('category_list', { title: 'Category List', category_list: list_categories });
       });
 };
 
